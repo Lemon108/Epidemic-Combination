7ad8362b1004c52e1d8d1524fdc9a9bc14033f6a
Pumpkin <klemon108@163.com>
2020-06-21 11:53:39 +0800

    test

--- epdimic/Group06/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class Group06Config(AppConfig):
-    name = 'Group06'
--- epdimic/Group06/users/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from . import models
-
-
-# Register your models here.
-# admin.register(models.UserInfo)
